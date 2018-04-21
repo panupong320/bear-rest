889e33d9ee600a9161b5d66878df181047bd1cfa
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-04-21 15:27:10 +0700

    redux store - basic

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+let reducer = function(state = [], action) {
+  if(action) {
+    if(action.type == 'FETCH-BEAR') {
+      return action.payload
+    }
+  }
+  return state
+}
+let store = createStoreWithMiddleware(reducer)
+
+let fetchBearActionCreator = function() {
+  return (dispatch) => {
+    axios.get('http://localhost:8000/api/bears')
+      .then(result => {
+        dispatch({type: 'FETCH_BEAR', payload: result.data })
+      })
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -7,17 +30,17 @@ class App extends Component {
     this.state = {data: [] };
   }
   componentDidMount(){
-      axios.get('http://localhost:8000/api/bears')
-      .then(result => {
-        this.setState({data: result.data })
-      })
+     store.subsscribe(()  => {
+       this.setState({data: store.getState()})
+     })
+     store.dispatch(fetchBearActionCreator());
   }
   render() {
     let bears = this.state.data;
     return (
       <div>
         {
-          bears.map(bear => <div>{bear.name}</div>)
+          bears.map(bear => <div key = {bear.id}> {bear.name} </div>)
         }
       </div>
     );
