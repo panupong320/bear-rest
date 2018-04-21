3df432940133b7c607ca59f60aa6c740ff0a330b
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-04-21 16:30:45 +0700

    use react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducer';
-import { fetchBear } from './actions';
+import { connect } from 'react-redux';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-
-let store = createStoreWithMiddleware(reducers)
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {data: [] };
-  }
-  componentDidMount(){
-     store.subscribe(()  => {
-       this.setState({data: store.getState().bear})
-     })
-     store.dispatch(fetchBear());
   }
   render() {
-    let bears = this.state.data;
+    let bears = this.props.bear;
     return (
       <div>
         {
@@ -32,4 +18,7 @@ class App extends Component {
   }
 }
 
-export default App;
+let mapStateToProps = (state) => {
+  {bear: state.bear}
+}
+export default connect(mapStateToProps)(App);
